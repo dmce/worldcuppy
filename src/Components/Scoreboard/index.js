84a5f84c8bfaa51b8c225fd97261be38d126f252
David McElroy <david.mcelroy@scotland.pnn.police.uk>
2018-06-11 15:20:28 +0100

    moved components and added error handling

---
+++ src/Components/Scoreboard/index.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import ScoreboardHelper from '../..//Helpers/scoreboard';
+
+import { withStyles } from '@material-ui/core/styles';
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableRow,
+  TableCell,
+  Typography,
+  CircularProgress,
+  Fade,
+  Card,
+  CardContent,
+} from '@material-ui/core';
+
+const styles = theme => ({
+  p: {
+    padding: 10,
+  },
+  card: theme.mixins.gutters({
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor: theme.palette.primary[50],
+  }),
+});
+
+class Scoreboard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      hasError: false,
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const scoreboardData = await ScoreboardHelper.get();
+
+      this.setState({
+        scoreboardData: scoreboardData.scoreboard,
+        isLoading: false,
+      });
+    } catch (error) {
+      this.setState({
+        vertical: 'bottom',
+        horizontal: 'right',
+        isLoading: false,
+        hasError: true,
+      });
+    }
+  }
+
+  render() {
+    const { scoreboardData, isLoading, hasError } = this.state;
+    const { classes } = this.props;
+
+    return (
+      <React.Fragment>
+        <Typography variant="display2">Scoreboard</Typography>
+        {isLoading && (
+          <Fade
+            in={isLoading}
+            style={{
+              transitionDelay: isLoading ? '800ms' : '0ms',
+            }}
+            unmountOnExit
+          >
+            <CircularProgress />
+          </Fade>
+        )}
+        {hasError && (
+          <React.Fragment>
+            <Typography color="textSecondary" variant="body1">
+              Error: Unable to display scoreboard. Issue has been logged
+            </Typography>
+          </React.Fragment>
+        )}
+        {!hasError &&
+          !isLoading &&
+          scoreboardData &&
+          scoreboardData.length > 0 && (
+            <Card className={classes.card}>
+              <CardContent>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Player</TableCell>
+                      <TableCell>Picks</TableCell>
+                      <TableCell>Points</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {scoreboardData.map((row, index) => (
+                      <TableRow key={index}>
+                        <TableCell>{row._id.username}</TableCell>
+                        <TableCell>{row.Picks}</TableCell>
+                        <TableCell>{row.Points}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </CardContent>
+            </Card>
+          )}
+        {!hasError &&
+          !isLoading &&
+          scoreboardData &&
+          scoreboardData.length > 0 && (
+            <Typography color="textSecondary" variant="body1">
+              No matches have finished
+            </Typography>
+          )}
+      </React.Fragment>
+    );
+  }
+}
+
+export default withStyles(styles)(Scoreboard);
