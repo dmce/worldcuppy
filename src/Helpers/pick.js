c459821230d0f5a1db8c41530140c9181db674e6
David McElroy <david.mcelroy@scotland.pnn.police.uk>
2018-06-12 07:29:37 +0100

    get points and resolved from db

--- src/Helpers/pick.js
+++ src/Helpers/pick.js
@@ -1,30 +1,40 @@
 import { default as api } from '../Services/api';
 
 export default class Pick {
-  constructor(id, fixtureId, outcome, gameday, user = localStorage.getItem('sub'), username = localStorage.getItem('name')) {
+  constructor(
+    id,
+    fixtureId,
+    outcome,
+    gameday,
+    points = 0,
+    resolved = false,
+    user = localStorage.getItem('sub'),
+    username = localStorage.getItem('name')
+  ) {
     this.Id = id;
     this.fixtureId = fixtureId;
     this.outcome = outcome;
     this.gameday = gameday;
     this.user = user;
-    this.username = username
-    this.resolved = false;
-    this.points = 0
+    this.username = username;
+    this.resolved = resolved;
+    this.points = points;
   }
 
   inPlayPoints(outcome) {
     if (!this.resolved) {
-    switch (outcome) {
-      case 'd':
-        this.points = 1;
-        break;
-      case this.outcome:
-        this.points = 3;
-        break;
-      default:
-        this.points = 0;
-        break;
-    }}
+      switch (outcome) {
+        case 'd':
+          this.points = 1;
+          break;
+        case this.outcome:
+          this.points = 3;
+          break;
+        default:
+          this.points = 0;
+          break;
+      }
+    }
   }
 
   static add(body) {
