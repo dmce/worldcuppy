f7992562d686726a4ca8ee4f1b1132d5af935573
David McElroy <david.mcelroy@scotland.pnn.police.uk>
2018-06-13 07:48:53 +0100

    add finished pick count

--- src/Helpers/scoreboard.js
+++ src/Helpers/scoreboard.js
@@ -1,11 +1,12 @@
 import { default as api } from '../Services/api';
 
 export default class Scoreboard {
-  constructor(user, username, points, picks) {
+  constructor(user, username, points, totalPicks, finishedPicks) {
     this.user = user;
     this.username = username;
     this.points = points;
-    this.picks = picks;
+    this.totalPicks = totalPicks;
+    this.finishedPicks = finishedPicks;
   }
 
   static get(Id) {
