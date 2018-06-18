b11e34ed629a64fadd76dd9c262f70c993359b13
David McElroy <david.mcelroy@scotland.pnn.police.uk>
2018-06-18 08:02:56 +0100

    refactor. add specific time frame api requests. better error handling

--- src/Helpers/fixture.js
+++ src/Helpers/fixture.js
@@ -25,7 +25,122 @@ export default class Fixture {
           : 'd';
   }
 
-  static getAll(competition) {
-    return footballDataApi.getFixtures(competition);
+  static processFixtures = data => {
+    let activeGameday = 0;
+
+    let fixtures = [];
+
+    data.fixtures.forEach((f, i) => {
+      const fixture = new Fixture(
+        f.homeTeamName,
+        f.awayTeamName,
+        f.status,
+        f.date,
+        f.matchday,
+        f._links.self.href
+      );
+
+      // TODO: REMOVE THIS
+      // if (i === 0) {
+      //   fixture.status = 'FINISHED';
+      //   fixture.goals(4, 3);
+      // }
+
+      // if (i === 1) {
+      //   fixture.status = 'IN_PLAY';
+      //   fixture.goals(1, 0);
+      // }
+
+      // if (i === 2) {
+      //   fixture.status = 'IN_PLAY';
+      //   fixture.goals(0, 3);
+      // }
+
+      // TODO: END
+
+      if (fixture.status === 'IN_PLAY') activeGameday = fixture.gameday;
+      if (fixture.gameday === activeGameday && fixture.status === 'TIMED')
+        fixture.status = 'GAMEDAY_ACTIVE';
+
+      switch (fixture.status) {
+        case 'FINISHED':
+          fixture.goals(f.result.goalsHomeTeam, f.result.goalsAwayTeam);
+          fixtures.push(fixture);
+          break;
+        case 'IN_PLAY':
+        case 'GAMEDAY_ACTIVE':
+        case 'GAMEDAY_FINISHED':
+          fixture.goals(f.result.goalsHomeTeam, f.result.goalsAwayTeam);
+          fixtures.push(fixture);
+          break;
+        case 'TIMED':
+          fixtures.push(fixture);
+          break;
+        default:
+          fixtures.push(fixture);
+          break;
+      }
+    });
+
+    return fixtures;
+  };
+
+  static async getAll(competition) {
+    const data = await footballDataApi.getFixtures(competition);
+    return new Promise((resolve, reject) => {
+      try {
+        resolve(this.processFixtures(data));
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+
+  static async getToday(competition) {
+    const data = await footballDataApi.getFixtures(
+      competition,
+      '?timeFrame=n1'
+    );
+    return new Promise((resolve, reject) => {
+      try {
+        resolve(this.processFixtures(data));
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+
+  static async getScheduled(competition, filterDays = 1) {
+    const tomorrow = startOfTomorrow();
+    const startFilter = format(tomorrow, 'YYYY-MM-dd');
+    const endFilter = addDays(tomorrow, filterDays);
+    const data = await footballDataApi.getFixtures(
+      competition,
+      `?timeFrameStart=${startFilter}&timeFrameEnd=${endFilter}`
+    );
+    return new Promise((resolve, reject) => {
+      try {
+        resolve(this.processFixtures(data));
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+
+  static async getFinished(competition, filterDays = 1) {
+    const yesterday = startOfYesterday();
+    const endFilter = format(yesterday, 'YYYY-MM-dd');
+    const startFilter = subDays(yesterday, filterDays);
+    const data = await footballDataApi.getFixtures(
+      competition,
+      `?timeFrameStart=${startFilter}&timeFrameEnd=${endFilter}`
+    );
+    return new Promise((resolve, reject) => {
+      try {
+        resolve(this.processFixtures(data));
+      } catch (error) {
+        reject(error);
+      }
+    });
   }
 }
